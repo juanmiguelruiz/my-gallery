0992418ccae94235ef60b912734747865a4f8ebd
Juan Miguel <juanmiguelruizpazos@gmail.com>
2020-11-05 17:31:50 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -29,15 +29,7 @@ export default {
 
 <style>
 @import url(
-  https://fonts.googleapis.com/css?family=Montserrat:100,
-  200,
-  300,
-  400,
-  500,
-  600,
-  700,
-  800,
-  900
+  https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900
 );
 
 .selector-for-some-widget {
